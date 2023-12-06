6dede58aff30599ce54518c07369c801cca0a617
otaviopedbot <otavio.santos@pedbot.com.br>
2023-12-06 13:25:45 -0300

    update

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,17 @@ router.get('/register', (req,res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async(req,res) => {
+router.post('/register', catchAsync(async(req, res, next) => {
     try{
         const {email, username, password} = req.body;
         const user = new User({email, username});
         const registeredUser = await User.register(user,password);
-        req.flash('success','Wellcome to Yelp Camp!')
-        res.redirect('/campgrounds')    
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success','Wellcome to Yelp Camp!')
+            res.redirect('/campgrounds')    
+        })
+
     }catch(e){
         req.flash('error', e.message);
         res.redirect('register')
@@ -29,5 +33,15 @@ router.post('/login', passport.authenticate('local', {failureFlash: true, failur
     req.flash('success', 'welcome back!');
     res.redirect('/campgrounds');
 })
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+}); 
 
 module.exports = router;
