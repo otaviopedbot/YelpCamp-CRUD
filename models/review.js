fe8ce942518c1e059b676452bf0e60680a0c1596
otaviopedbot <otavio.santos@pedbot.com.br>
2023-12-06 17:52:34 -0300

    update

--- models/review.js
+++ models/review.js
@@ -5,6 +5,10 @@ const schema = mongoose.Schema;
 const reviewSchema = new schema({
     body: String,
     rating: Number,
+    author: {
+        type: schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
